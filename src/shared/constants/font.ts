17d2da3c6be18eea8f3273408ab8eb430e43eae6
Prince Kunal <princekunal652@gmail.com>
2025-12-06 00:09:31 +0530

    new fonts added

---
+++ src/shared/constants/font.ts
@@ -0,0 +1,28 @@
+export const FigtreeFont = {
+  regular: 'Figtree-Regular',
+  bold: 'Figtree-Bold',
+  extraBold: 'Figtree-ExtraBold',
+  semiBold: 'Figtree-SemiBold',
+  medium: 'Figtree-Medium',
+  light: 'Figtree-Light',
+  italic: 'Figtree-Italic',
+  boldItalic: 'Figtree-BoldItalic',
+  extraBoldItalic: 'Figtree-ExtraBoldItalic',
+  semiBoldItalic: 'Figtree-SemiBoldItalic',
+  mediumItalic: 'Figtree-MediumItalic',
+  lightItalic: 'Figtree-LightItalic',
+};
+
+export const LeagueGauthicFont = {
+  regular: 'LeagueGothic-Regular',
+  condensed: 'LeagueGothic_Condensed-Regular',
+  semiCondensed: 'LeagueGothic_SemiCondensed-Regular',
+};
+
+export const QuickSandFont = {
+  regular: 'Quicksand-Regular',
+  bold: 'Quicksand-Bold',
+  light: 'Quicksand-Light',
+  medium: 'Quicksand-Medium',
+  semiBold: 'Quicksand-SemiBold',
+}
